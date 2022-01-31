a2691874ff66b9c50800570ec60ff0c99e7e59a0
idreesalmasri <idrees.massri95@gmail.com>
2022-01-31 08:50:11 +0200

    i didnot finish working on this task

---
+++ server.js
@@ -0,0 +1,23 @@
+`use strict`;
+const express = require('express');
+const app = express();
+const data = require('./data.json');
+function Data(title, poster_path, overview) {
+    this.title = title;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+function homePage(req, res) {
+    let newData = new Data(jsonData.title, jsonData.poster_path, jsonData.overview)
+    return res.status(200).json(newData);
+}
+function favorite(req, res) {
+    return res.status(200).send("Welcome to Favorite Page");
+}
+app.get("/", homePage);
+app.get("/favorite", favorite);
+
+
+app.listen(3200, () => {
+    console.log("hello");
+})
