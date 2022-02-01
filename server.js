52919f77ea555e4cc76fd4eed5fb5d1ad8188725
idreesalmasri <idrees.massri95@gmail.com>
2022-02-01 08:46:54 +0200

    i didnot finish

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
 `use strict`;
 const express = require('express');
 const app = express();
-const data = require('./data.json');
-
-function Data(title, poster_path, overview) {
+// const data = require('./data.json');
+const axios= require('axios');
+const dotenv=require('dotenv');
+dotenv.config();
+const APIKEY=process.env.APIKEY;
+function Data(id,title,release_date, poster_path, overview) {
+    this.id=id;
     this.title = title;
-    this.poster_path = poster_path;
+    this.releaseDate=release_date;
+    this.posterPath = poster_path;
     this.overview = overview;
 }
-function homePage(req, res) {
-    let newData = new Data(data.title, data.poster_path, data.overview)
-
-    return res.status(200).json(newData);
+function trending(req, res) {
+    let trending =[];
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US&number=10`).then(value =>{
+        // console.log(value.data)
+        value.data.results.forEach(result =>{
+            let oneResult = new Data(result.id,result.title,result.release_date,result.poster_path,result.overview);
+            
+            trending.push(oneResult);
+            
+        }).catch(function (error) {
+            console.log(error);
+            res.sendStatus(500).send("error: " + error)
+        });
+        return res.status(200).json(trending);
+    })
+    
 }
-function favorite(req, res) {
-    return res.status(200).send("Welcome to Favorite Page");
+function search(req, res) {
+    let search=[];
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=The&page=2`).then(searchValue=>{
+    searchValue.data.results.forEach(result =>{
+        let searchResult=new Data(result.id,result.title,result.release_date,result.poster_path,result.overview);
+        search.push(searchResult);
+    }).catch(function (error) {
+        console.log(error);
+        res.sendStatus(500).send("error: " + error)
+    });
+    return res.status(200).json(search);
+    })
 }
-app.get("/", homePage);
-app.get("/favorite", favorite);
+app.get("/trending", trending);
+app.get("/search", search);
 
 // app.use(function(err,req,tes,text){
 //     console.error(err.stack);
@@ -33,6 +60,6 @@ app.get("/favorite", favorite);
 //     res.send('Sorry, something went wrong 404');
 // })
 
-app.listen(3200, () => {
+app.listen(3320, () => {
     console.log("hello");
 })
