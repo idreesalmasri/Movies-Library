d175ea18ec6fbb195be2a592bfd31b4c754763d8
idreesalmasri <idrees.massri95@gmail.com>
2022-02-02 22:20:23 +0200

    done

---
+++ schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS myFavorite;
+
+
+
+CREATE TABLE IF NOT EXISTS myFavorite(
+id SERIAL PRIMARY KEY ,
+title VARCHAR(255),
+releaseDate INTEGER,
+posterPath VARCHAR(1000),
+overview VARCHAR(10000),
+comment VARCHAR(1000)
+
+)
